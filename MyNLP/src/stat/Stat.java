369409b9a997448ef2515c1106dc99f7786b74fb
Jiachen Li <jiacli.jk@gmail.com>
2015-01-22 00:57:56 -0500

    Update getStat

--- MyNLP/src/stat/Stat.java
+++ MyNLP/src/stat/Stat.java
@@ -1,7 +1,7 @@
 /**
  * Program used to calculated statistics for given documents
  */
-package langStat;
+package stat;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
@@ -16,34 +16,34 @@ import java.util.List;
  * @author Jiachen
  *
  */
-public class langStat {
+public class Stat {
 
     /**
      * @param args
      */
     public static void main(String[] args) {
-        
 
     }
-    
+
     /**
-     * @param filePath (plain text)
+     * @param filePath
+     *            (plain text)
      * @return tokenList
      * @throws IOException
      */
     public List<String> getTokens(String filePath) throws IOException {
         List<String> tokenList = new ArrayList<>();
-        
+
         BufferedReader br = new BufferedReader(new InputStreamReader(
                 new FileInputStream(filePath)));
-        
+
         String line;
         while ((line = br.readLine()) != null) {
             line = line.trim();
             if (line.length() == 0) {
                 continue;
             }
-            
+
             String[] tokens = line.split("\\s+");
             for (int i = 0; i < tokens.length; i++) {
                 tokens[i] = tokens[i].replaceAll("\\W", "").toLowerCase();
@@ -54,21 +54,48 @@ public class langStat {
         }
         br.close();
 
-        return tokenList;        
+        return tokenList;
     }
-    
-    /** Get the statistics from the tokens
+
+    /**
+     * Get the statistics from the tokens
      * 
-     * @param tokenList
-     * @return hashMap
+     * @param tokens
+     * @return statMap
      */
     public HashMap<String, Float> getStats(List<String> tokens) {
-        HashMap<String, Float> map = new HashMap<>();
-        
-        HashSet<String> set = new HashSet<>();
-        
-        
-        return map;
+        HashMap<String, Float> stat = new HashMap<>();
+
+        HashMap<String, Integer> word_map = new HashMap<>();
+        HashSet<String> word_set = new HashSet<>();
+        HashMap<String, Integer> bigram_map = new HashMap<>();
+        HashSet<String> bigram_set = new HashSet<>();
+
+        int[] letterCount = new int[26];
+
+        // # word & bigram
+        for (int i = 0; i < tokens.size(); i++) {
+            String word = tokens.get(i);
+
+            if (!word_set.contains(word)) {
+                word_set.add(word);
+                word_map.put(word, 1);
+            } else {
+                word_map.put(word, word_map.get(word) + 1);
+            }
+
+            if (i != tokens.size() - 1) {
+                String bigram = tokens.get(i) + " " + tokens.get(i + 1);
+                if (!bigram_set.contains(bigram)) {
+                    bigram_set.add(bigram);
+                    bigram_map.put(bigram, 1);
+                } else {
+                    bigram_map.put(bigram, bigram_map.get(word) + 1);
+                }
+            }
+        }
+
+        return stat;
     }
 
 }
